c9d685f85270cfcb4eef34c1963202cafff4bed9
David Bureš <buresdv@gmail.com>
2023-05-01 18:35:17 +0200

    Initial Commit

---
+++ Neo/NeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoApp.swift
+//  Neo
+//
+//  Created by David Bureš on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
